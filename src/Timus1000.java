22f9b1bf4ce7ca79b61b1572d8909ea4ce1f2c19
Tri Truong <triqtruong@gmail.com>
2017-06-18 20:17:19 -0400

    Timus: 1000
    Change-Id: I4853a9949fba9bc386b53967dbc75cb3d1ec4eac

--- src/Timus1000.java
+++ src/Timus1000.java
@@ -1,16 +1,27 @@
-/* */
+import java.util.Scanner;
 
-public class Timus1000 {
-    public int sum(int var_a, int var_b) {
-        int output = var_a + var_b;
-        return output;
+/**
+ *  Class created to solve problem 1000 - Timus. Can be tested with console.
+ *  Class includes: sum - return result of summation between 2 numbers
+ *                  main - console program used to test the sum function
+ */
+public final class Timus1000 {
+    /**
+     * Sum() - Adding 2 integers together
+     * @param first: first integer to be entered
+     * @param second: second integer to be entered
+     * @return: integer value of summation
+     */
+    public int sum(int first, int second) {
+        return first + second;
     }
 
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         Timus1000 math = new Timus1000();
-        System.out.println(math.sum(2,3));
+        try (Scanner scanner = new Scanner(System.in)) {
+            int first = scanner.nextInt();
+            int second = scanner.nextInt();
+            System.out.println(math.sum(first, second));
+        }
     }
 }
-
-
-
