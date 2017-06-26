503b0e1511c78138f0a58c39cdedec3ed861fb46
Tri Truong <triqtruong@gmail.com>
2017-06-25 20:24:34 -0700

    Sum values in an array
    Change-Id: I43c0155c3a72769be5c55ac14055f2fde85da5fd

--- test/ArrayPracticeTest.java
+++ test/ArrayPracticeTest.java
@@ -3,6 +3,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ArrayPracticeTest {
     @Test
+    public void sumArray_multipleValues() {
+        double[] values = {1, 2, 3, 4};
+        double sum = ArrayPractice.sumArray(values);
+        assertEquals(10, sum);
+    }
+
+    @Test
+    public void sumArray_oneValue() {
+        double[] values = {4};
+        double sum = ArrayPractice.sumArray(values);
+        assertEquals(4, sum);
+    }
+
+    @Test
+    public void sumArray_emptyArray() {
+        double[] values = new double[0];
+        double sum = ArrayPractice.sumArray(values);
+        assertEquals(0, sum);
+    }
+
+    @Test
     public void avgArray_multipleValues() {
         double[] values = {1, 2, 3, 4};
         double average = ArrayPractice.avgArray(values);
