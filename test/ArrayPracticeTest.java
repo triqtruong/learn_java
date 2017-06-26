b240c17035548b033bfac2d346aec4ad889231c2
Tri Truong <triqtruong@gmail.com>
2017-06-25 23:39:22 -0700

    Find location of a value
    Change-Id: I64b100904ba2f7721e918d3e3a398bcb1bec2336

--- test/ArrayPracticeTest.java
+++ test/ArrayPracticeTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ArrayPracticeTest {
@@ -35,5 +36,13 @@ class ArrayPracticeTest {
         double[] values = {4};
         double average = ArrayPractice.avgArray(values);
         assertEquals(4, average);
+    }
+
+    @Test
+    public void searchIndex_multipleElements() {
+        double[] values = {0, 1, 2, 3, 4, 1, 2};
+        int[] indexLocation = ArrayPractice.searchIndex(1, values);
+        int[] expected = {1, 5};
+        assertArrayEquals(expected, indexLocation);
     }
 }
