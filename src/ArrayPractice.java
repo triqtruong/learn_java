4e0a53b31bb131218b0d31478fe9b69aef2ffac1
Tri Truong <triqtruong@gmail.com>
2017-07-02 15:20:01 -0700

    Find the duplicated element in strings Problem 12
    Change-Id: I7381dc90ad9704b8010f8c224b53cc089f1862e0

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * Class created to solve problems of w3resource
@@ -94,5 +95,28 @@ public final class ArrayPractice {
             reversed[i] = values[values.length - i - 1];
         }
         return reversed;
+    }
+
+    /**
+     * Find the duplicated element of {@code strings}.
+     * @param strings: the given string.
+     * @return the duplicated elements.
+     */
+    public static String[] duplicateString(String[] strings) {
+        HashMap<String, Boolean> check = new HashMap<String, Boolean>();
+        String[] duplicates = new String[strings.length];
+        int duplicatesSize = 0;
+        for (String key : strings) {
+            if ((check.containsKey(key))) {
+                Boolean added = (Boolean) check.get(key);
+                if (!added) {
+                    duplicates[duplicatesSize++] = key;
+                }
+                check.put(key,true);
+            } else {
+                check.put(key,false);
+            }
+        }
+        return Arrays.copyOf(duplicates,duplicatesSize);
     }
 }
